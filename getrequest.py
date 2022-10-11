5abd414cf1ccd676aa20d3682f911814e15185f1
Courtesi <c.chang673@gmail.com>
2022-10-11 03:27:04 -0700

    initial commit

---
+++ getrequest.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+def get_request() -> list:
+    res = []
+    CLIENT_ID = 'd1b6de05b5d14675f54d1772d7804985'
+
+    username = input("Enter a username:")
+
+    url = f'https://api.myanimelist.net/v2/users/{username}/animelist?fields=list_status'
+
+    while True:
+        response = requests.get(url, headers = {'X-MAL-CLIENT-ID': CLIENT_ID})
+
+        response.raise_for_status()
+        anime_list = response.json()
+        response.close()
+
+        for entry in anime_list["data"]:
+            res.append( (entry["node"]["id"], entry["node"]["title"], entry["list_status"]["score"]) )
+        
+        if "next" not in anime_list["paging"]:
+            break
+            
+        url = anime_list["paging"]["next"]
+
+    res.sort(key = lambda x: x[2], reverse = True)
+
+    with open("useranimelist.json", "w") as file:
+        json.dump(res, file)
+    
+    return res
+
+# if __name__ == "__main__":
+#     get_request()
---
+++ main.py
@@ -0,0 +1,4 @@
+from algorithmsearch import createRecommendations
+
+if __name__ == "__main__":
+    createRecommendations()
